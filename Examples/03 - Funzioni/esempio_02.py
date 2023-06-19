48d9b708946585f23222eb1fa4728846e2e1c745
Romolo Politi <romolo.politi@inaf.it>
2023-06-19 22:51:21 +0200

    add examples

---
+++ Examples/03 - Funzioni/esempio_02.py
@@ -0,0 +1,23 @@
+def decor(funct):
+    def wrapper(*args, **kwargs):
+        print("##################################")
+        a=funct(*args, **kwargs)
+        print("##################################")
+        return a
+    return wrapper
+    
+
+
+@decor
+def test01(name: str, age: int)->str:
+    print(f"{name} is {age} years old")
+    
+@decor
+def test02():
+    """TEST 02"""
+    print("test2")
+
+
+def test03():
+    """TEST 03"""
+    print("test3")
